4fb16180f23ed9d4f246971de4e7b6144d094962
vanyasree123 <vinnusaruvu@gmail.com>
2025-01-29 08:40:55 -0800

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,14 @@
+public class Basic{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        int a=20;
+        int b=30;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(b/a);
+        System.out.println(a%b);
+    }
+}
